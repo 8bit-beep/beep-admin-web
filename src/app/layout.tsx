7a74280bb3338f499a0ed39d5a137c346a01f681
김태우 <taewoo080401@gmail.com>
2026-01-19 18:53:48 +0900

    feat: init

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { BdsRegistry, ModalProvider } from "@bds-web/ui";
+import { LoadingBar } from "@cher1shrxd/loading";
+import { colors } from "@bds-web/colors";
+
+export const metadata: Metadata = {
+  title: "삑 어드민",
+  description: "삑 관리를 위한 어드민 페이지",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className="antialiased">
+        <LoadingBar color={colors.blue.light} />
+        <ModalProvider />
+        <BdsRegistry>{children}</BdsRegistry>
+      </body>
+    </html>
+  );
+}
