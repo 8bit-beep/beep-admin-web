d0441ff84a605693442d86ad23a70754662d2dab
김태우 <taewoo080401@gmail.com>
2026-01-22 12:26:16 +0900

    fix: not check in auth page

--- src/shared/provider/AuthProvider.tsx
+++ src/shared/provider/AuthProvider.tsx
@@ -1,16 +1,22 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import { getAccessToken } from "../libs/cookie";
+import { useEffect } from "react";
 
 const AuthProvider = () => {
+  const pathname = usePathname();
+
   const getToken = async () => {
     const accessToken = await getAccessToken();
-    if (!accessToken && typeof window !== "undefined") {
+    if (!accessToken && typeof window !== "undefined" && !pathname.startsWith("/callback") && pathname !== "/login") {
       window.location.href = "/login";
     }
   };
 
-  getToken();
+  useEffect(() => {
+    getToken();
+  }, [pathname]);
 
   return null;
 };
