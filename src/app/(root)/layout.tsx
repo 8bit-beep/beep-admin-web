7585fe69991e16dd4b6cc8788ed0a9d343dd3940
jdw09 <razion0304@naver.com>
2026-02-06 19:56:50 +0900

    fix: add missing AuthProvier

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import SearchBar from "@/features/search/ui/SearchBar";
+import AuthProvider from "@/shared/provider/AuthProvider";
 import Header from "@/widgets/header/ui/Header";
 import Sidebar from "@/widgets/sidebar/ui/Sidebar";
 import Tabbar from "@/widgets/tabbar/ui/Tabbar";
@@ -7,6 +8,7 @@ import { PropsWithChildren } from "react";
 export default function MainLayout({ children }: PropsWithChildren) {
   return (
     <div className="w-full h-svh flex items-start flex-col xl:flex-row overflow-hidden">
+      <AuthProvider />
       <Sidebar />
       <Header />
       <main className="w-full flex-1 h-[calc(100%-147px)] xl:h-full px-4 xl:px-[70px] xl:pt-13 pb-1 xl:pb-5 flex flex-col gap-4.5 overflow-y-auto">
