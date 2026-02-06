413935bacf84fe98d2fd6dd8275757d360b4f011
jdw09 <razion0304@naver.com>
2026-02-06 20:32:37 +0900

    feat: root path redirection in mobile header

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -2,11 +2,14 @@
 
 import SearchBar from "@/features/search/ui/SearchBar";
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="w-full flex items-center gap-4 pt-16 mb-6 px-4 xl:hidden shrink-0">
-      <Image width={40} height={40} src="/logo.svg" alt="logo" />
+      <Link href="/">
+        <Image width={40} height={40} src="/logo.svg" alt="logo" />
+      </Link>
       <SearchBar onlyMobile={true} />
     </header>
   );
